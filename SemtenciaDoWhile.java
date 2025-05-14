aa5508c38512e3d9d53567ecca45ba71b91c75b0
Mrsmemi24 <20243rd027@utez.edu.mx>
2025-05-14 17:48:55 -0600

    no sale

---
+++ SemtenciaDoWhile.java
@@ -0,0 +1,16 @@
+public class SemtenciaDoWhile{
+    public static void main (String[] args) {
+        int contador = 1;
+        do {
+            System.out.println (contador);
+            contador++;
+        } while(contador);
+        do{
+            String respuesta = System.out.println("Quieres continuar?");
+            System.out.println("(s: si n: no)");
+        } while (respuesta = n);
+               System.out.println("Finalizando");
+    
+
+    }
+}
